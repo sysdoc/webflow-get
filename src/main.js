0f8997b202a8ca5974b02615592630c73c946048
Viktor <thetravlrd@gmail.com>
2022-11-07 11:23:45 +0100

    fix sitemap.xml indentation

--- src/main.js
+++ src/main.js
@@ -4,16 +4,16 @@ function sitemapXmlFromUrls(fullUrls) {
   const date = new Date();
   const dateStr = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
 
-  return (`
-    <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ` + fullUrls.map((fullUrl) => `
-        <url>
-            <loc>${fullUrl}</loc>
-            <lastmod>${dateStr}</lastmod>
-        </url>
-    `) + `
-    </urlset>
-  `);
+  return (
+    `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">` +
+    fullUrls.map((fullUrl) => `
+      <url>
+          <loc>${fullUrl}</loc>
+          <lastmod>${dateStr}</lastmod>
+      </url>`
+    ) +
+    `</urlset>`
+  );
 }
 
