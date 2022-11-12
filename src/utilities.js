8e3217c66835f266743f26e55e34d8b74cb895f3
Viktor <thetravlrd@gmail.com>
2022-11-12 09:21:01 +0100

    sitemap and robots.txt

--- src/utilities.js
+++ src/utilities.js
@@ -207,23 +207,23 @@ export async function updateSnapshot() {
   await storeTextContentIntoFile(robotsTxt, "public/robots.txt");
 };
 
-
 function sitemapXmlFromUrls(fullUrls) {
   const date = new Date();
-  const dateStr = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+  const dateStr = date.toISOString();
 
-  return (`
-    <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ` + fullUrls.map((fullUrl) => `
-        <url>
-            <loc>${fullUrl.replace(config.webflowSiteBaseUrl, config.productionSiteBaseUrl)}</loc>
-            <lastmod>${dateStr}</lastmod>
-        </url>
-    `).join("") + `
-    </urlset>
-  `);
+  return (
+    `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">` +
+    fullUrls.map((fullUrl) => `
+      <url>
+          <loc>${fullUrl}</loc>
+          <lastmod>${dateStr}</lastmod>
+      </url>`
+    ).join("") +
+    `</urlset>`
+  );
 }
+
 
 const robotsTxt = `
 User-agent: Googlebot
@@ -231,4 +231,6 @@ Disallow: /nogooglebot/
 
 User-agent: *
 Allow: /
+
+Sitemap: ${config["productionSiteBaseUrl"]}/sitemap.xml
 `;
